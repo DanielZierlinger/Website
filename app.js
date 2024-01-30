dd5f7dbe68ec1aa916245d17ea52b79806f138c2
daniel.zierlinger <daniel.zierlinger@tutanota.com>
2024-01-30 01:28:26 +0100

    Added section with scroll

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-function alertButton(){
-    alert("Hello World");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")){
+
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
 }
